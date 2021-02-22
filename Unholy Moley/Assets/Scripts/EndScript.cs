6a8a7bf525558c88463eca33055cf1ea5249dfa5
Wi024933 <beattywf@knights.ucf.edu>
2021-02-22 17:25:56 -0500

    Menus Added

---
+++ Unholy Moley/Assets/Scripts/EndScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndScript : MonoBehaviour
+{
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game!");
+
+        Application.Quit();
+    }
+
+}
---
+++ Unholy Moley/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+
+    public AudioSource aSource;
+    public AudioClip button;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
