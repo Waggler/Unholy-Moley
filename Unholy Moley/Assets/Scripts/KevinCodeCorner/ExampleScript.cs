214050aac7d26c244d4089690e1f3e96d6f972ea
Waggler <khdavis4@gmail.com>
2021-02-23 18:45:31 -0500

    Quick Update
    added a script or two

---
+++ Unholy Moley/Assets/Scripts/KevinCodeCorner/ExampleScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleScript : MonoBehaviour
+{
+    [Header("Health Settings")]// A header creates a grouping in Editor for the things under it.
+    public int health = 0;
+    public int maxHealth = 100;// a public variable is shown in the Editor and is accessible by other scripts
+    private int lives;// a private variable is NOT shown in the Editor and is NOT accessible by other scripts
+
+    [Header("Shield Settings")]
+    public int shield = 0;
+    public int maxShield = 0;
+
+    [Header("Movement Settings")]
+    [SerializeField] private float currentSpeed;// a SerialeField private variable is shown in the Editor BUT IS NOT accessible by other scripts
+    private float maxSpeed;
+    private float jumpHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+    void LateUpdate()
+    {
+
+    }
+}
