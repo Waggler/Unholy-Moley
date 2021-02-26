ab965aebaedd52046b92b855e1fbfa2048a34483
Wi024933 <beattywf@knights.ucf.edu>
2021-02-26 18:01:45 -0500

    walls are higher

---
+++ Unholy Moley/Assets/Scripts/Speedvialscript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speedvialscript : MonoBehaviour
+{
+    [Header("Stat Increase")]
+
+    public float boostAmount = 1.5f;
+
+    [Header("Outside Objects")]
+
+    public GameObject vialCloud;
+
+    // Checks whether the player comes in contact
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Boost(other);
+        }
+    }
+
+    // I am SPEED
+    void Boost(Collider player)
+    {
+        Instantiate(vialCloud, transform.position, transform.rotation);
+
+        
+
+        Destroy(gameObject);
+    }
+
+}
