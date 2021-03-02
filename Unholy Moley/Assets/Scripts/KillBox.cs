783472262287a7871ef0231d4aadd8fd5cc5d467
Waggler <khdavis4@gmail.com>
2021-03-02 16:21:52 -0500

    Code Update
    Enemy kills Player, and Gun pickup zone is implemented

---
+++ Unholy Moley/Assets/Scripts/KillBox.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillBox : MonoBehaviour
+{
+    public GameObject Gun;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Killbox"))
+        {
+            Destroy(gameObject);
+        }
+        if (other.CompareTag("GunPickup"))
+        {
+            Gun.SetActive(true);
+        }
+    }
+}
--- Unholy Moley/Assets/Scripts/MoleScripts/MoleScript.cs
+++ Unholy Moley/Assets/Scripts/MoleScripts/MoleScript.cs
@@ -6,20 +6,19 @@ using UnityEngine.AI;
 public class MoleScript : MonoBehaviour
 {
 
+    [Header("ChasingPlayer")]
     public float lookRadius = 10f;
-
     Transform target;
     NavMeshAgent agent;
 
+    [Header("PatrollingRandom")]
+    //PatrollingWaypoints
     public LayerMask whatIsGround;
-
-    //Patrolling
-    private Transform[] patrolPoints;
-
-
     public Transform[] points;
     private int destPoint = 0;
 
+    [Header("PatrollingRandom")]
+    private Transform[] patrolPoints;
     public Vector3 walkPoint;
     public bool walkPointSet = false;
     public float walkPointRange;
@@ -38,12 +37,11 @@ public class MoleScript : MonoBehaviour
         agent.autoBraking = false;
 
         GotoNextPoint();
-    }
+    }//end Start
 
     // Update is called once per frame
     void Update()
     {
-
         float distance = Vector3.Distance(target.position, transform.position);
 
         if (distance <= lookRadius)
@@ -53,14 +51,16 @@ public class MoleScript : MonoBehaviour
             if (distance <= agent.stoppingDistance)
             {
                 FaceTarget();
-            }
-        }
+            }//end if
+        }//end if
         else
         {
             if (!agent.pathPending && agent.remainingDistance < 5f)
                 GotoNextPoint();
-        }
-    }
+        }//end else
+    }//end Update
+
+    //This method tells the Mole to travel to the next Waypoint
     void GotoNextPoint()
     {
         // Returns if no points have been set up
@@ -73,19 +73,20 @@ public class MoleScript : MonoBehaviour
         // Choose the next point in the array as the destination,
         // cycling to the start if necessary.
         destPoint = (destPoint + 1) % points.Length;
-    }
+    }//end GoToNextPoint
 
+    //This method Makes the Mole Randomly Patrol. Currently it is not used
     private void Patroling()
     {
         if (!walkPointSet)
         {
             SearchWalkPoint();
-        }
+        }//end if
 
         if (walkPointSet)
         {
             agent.SetDestination(walkPoint);
-        }
+        }//end if
 
         Vector3 distanceToWalkPoint = transform.position - walkPoint;
 
@@ -93,17 +94,10 @@ public class MoleScript : MonoBehaviour
         if (distanceToWalkPoint.magnitude < 5f)
         {
             walkPointSet = false;
-        }
-    }
-    private void MoveToPoint()
-    {
-        for (int i = 0; i <= patrolPoints.Length; i++)
-        {
-            agent.SetDestination(patrolPoints[i].transform.position);
-        }
+        }//end if
+    }//end Patrolling
 
-    }
-
+    //This method creates a random walkpoint within a set distance of the Mole. Currently it is not used
     private void SearchWalkPoint()
     {
         float randomZ = Random.Range(-walkPointRange, walkPointRange);
@@ -114,18 +108,31 @@ public class MoleScript : MonoBehaviour
         if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
         {
             walkPointSet = true;
-        }
-    }
+        }//end if
+    }//end SearchWalkPoint
 
+    //This method causes to Mole to face the player when in close range
     void FaceTarget()
     {
         Vector3 direction = (target.position - transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }//end FaceTarget
+
+    /*Old Waypoint Script
+ * private void MoveToPoint()
+{
+    for (int i = 0; i <= patrolPoints.Length; i++)
+    {
+        agent.SetDestination(patrolPoints[i].transform.position);
     }
+
+}*///End MoveToPoint
+
+    //This method shows the Mole's detection radius in Editor
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, lookRadius);
-    }
+    }//end OnDrawGizmosSelected
 }
