017ef01ba491498991dac9efffd229792f5b10d8
Nafis B. Taylor <nafis_taylor86@knights.ucf.edu>
2021-03-03 00:34:44 -0500

    MoleyFeetAudio

---
+++ Unholy Moley/Assets/Scripts/MoleScripts/MoleysFeet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MoleysFeet : MonoBehaviour
+{
+   [SerializeField] AudioClip[] audioClip;
+
+    public AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void MoleyStep()
+    {
+        AudioClip clip = GetRandomClip();
+        audioSource.PlayOneShot(clip);
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        int index = Random.Range(0, audioClip.Length - 1);
+        return audioClip[index];
+    }
+}
