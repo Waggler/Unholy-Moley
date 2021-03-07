8a9a8b931e70e58957c99f7a5d0e610705064bb6
Wi024933 <beattywf@knights.ucf.edu>
2021-03-07 12:25:36 -0500

    final touches?

---
+++ Unholy Moley/Assets/Scripts/PlayerScripts/QuitScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitScript : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            Application.Quit();
+            Debug.Log("Quit!");
+        }
+    }
+}
